191f6ff3473757fe9b925ace9741fec2af1f599c
MoonInHo <ansdlsgh55@gmail.com>
2024-02-26 14:07:56 +0900

    filst commit

---
+++ src/test/java/com/innovation/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovation.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
