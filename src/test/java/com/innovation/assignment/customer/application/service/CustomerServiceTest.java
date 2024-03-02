c8912ff36db9e2c33bb5f5c8d344579c264a35b6
MoonInHo <ansdlsgh55@gmail.com>
2024-03-02 19:40:05 +0900

    비밀번호 변경 및 고객 정보 삭제 테스트코드 추가

--- src/test/java/com/innovation/assignment/customer/application/service/CustomerServiceTest.java
+++ src/test/java/com/innovation/assignment/customer/application/service/CustomerServiceTest.java
@@ -1,14 +1,13 @@
 package com.innovation.assignment.customer.application.service;
 
-import com.innovation.assignment.exception.exception.customer.DuplicatePhoneException;
-import com.innovation.assignment.exception.exception.customer.DuplicateEmailException;
-import com.innovation.assignment.exception.exception.customer.EmptyCustomerListException;
-import com.innovation.assignment.exception.exception.customer.CustomerNotFoundException;
+import com.innovation.assignment.customer.domain.entity.Customer;
+import com.innovation.assignment.customer.domain.repository.CustomerHistoryRepository;
+import com.innovation.assignment.customer.domain.vo.Password;
+import com.innovation.assignment.customer.presentation.dto.request.*;
+import com.innovation.assignment.exception.exception.customer.*;
 import com.innovation.assignment.customer.application.dto.request.CreateCustomerRequestDto;
 import com.innovation.assignment.customer.domain.repository.CustomerRepository;
 import com.innovation.assignment.customer.infrastructure.dto.response.GetCustomerResponseDto;
-import com.innovation.assignment.customer.presentation.dto.request.SearchCustomerByEmailRequestDto;
-import com.innovation.assignment.customer.presentation.dto.request.SearchCustomerByPhoneRequestDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,12 +15,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -49,6 +50,12 @@ class CustomerServiceTest {
 
     @Mock
     private CustomerRepository customerRepository;
+
+    @Mock
+    private ApplicationEventPublisher eventPublisher;
+
+    @Mock
+    private CustomerHistoryRepository customerHistoryRepository;
 
     @InjectMocks
     private CustomerService customerService;
@@ -218,10 +225,67 @@ class CustomerServiceTest {
         assertThat(customerInfo).isNotNull();
         assertThat(customerInfo.getId()).isEqualTo(1L);
     }
-    //TODO 비밀번호 변경 -> 기존 비밀번호와 같은 문자열로 변경 시도시 예외 발생
-    //TODO 비밀번호 변경 -> 변결할 비밀번호와 비밀번호 확인이 서로 일치하지 않을때 예외 발생
-    //TODO 비밀번호 변경 -> 비밀번호 검증이 끝나면 비밀번호 변경
-    //TODO 고객정보 삭제 -> 해당 식별자를 가진 고객이 없을 경우 예외 발생
-    //TODO 고객정보 삭제 -> 고객 정보가 존재할 경우 고객 정보 삭제
-    //TODO 고객정보 삭제 -> 삭제된 고객 히스토리 생성 여부 검증
+
+    @Test
+    @DisplayName("비밀번호 변경 - 변경하려는 비밀번호가 기존 비밀번호와 동일할 경우 예외 발생")
+    void samePassword_changePassword_throwException() {
+        //given
+        Customer customer = createCustomerRequestDto.toEntity();
+        ChangePasswordRequestDto changePasswordRequestDto = new ChangePasswordRequestDto(
+                1L,
+                "testPassword123!",
+                "testPassword123!"
+        );
+
+        given(customerRepository.getCustomer(1L)).willReturn(Optional.of(customer));
+        given(passwordEncoder.matches(any(), any())).willReturn(true);
+
+        //when
+        Throwable throwable = catchThrowable(() -> customerService.changePassword(changePasswordRequestDto));
+
+        //then
+        assertThat(throwable).isInstanceOf(SamePasswordException.class);
+        assertThat(throwable).hasMessage("기존 비밀번호와 동일한 비밀번호로 변경할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("비밀번호 변경 - 올바른 비밀번호로 비밀번호 변경 시도시 비밀번호 업데이트")
+    void properNewPassword_changePassword_updatePassword() {
+        //given
+        Customer customer = createCustomerRequestDto.toEntity();
+        ChangePasswordRequestDto changePasswordRequestDto = new ChangePasswordRequestDto(
+                1L,
+                "newPassword123!",
+                "newPassword123!"
+        );
+
+        given(customerRepository.getCustomer(1L)).willReturn(Optional.of(customer));
+        given(passwordEncoder.encode(changePasswordRequestDto.newPassword())).willReturn("encodedNewPassword123!");
+        given(passwordEncoder.matches(any(), any())).willReturn(false);
+
+        //when
+        customerService.changePassword(changePasswordRequestDto);
+
+        //then
+        assertThat(customer.password()).isEqualTo("encodedNewPassword123!");
+    }
+
+    // 회원 정보 변경 -> @DynamicUpdate 를 이용해서 동적 업데이트를 수행하여 mocking 테스트로는 한계가 있음
+
+    @Test
+    @DisplayName("고객 정보 삭제 - 존재하는 고객의 id로 고객 정보 삭제 시도시 고객정보 삭제")
+    void existCustomerId_deleteCustomerInfo_deleteCustomerInfoAnd() { //TODO 비동기 이벤트 테스트 고민
+        //given
+        Customer customer = createCustomerRequestDto.toEntity();
+        DeleteCustomerRequestDto deleteCustomerRequestDto =
+                new DeleteCustomerRequestDto(1L, "테스트 탈퇴 사유");
+
+        given(customerRepository.getCustomer(any())).willReturn(Optional.ofNullable(customer));
+
+        //when
+        customerService.deleteCustomer(deleteCustomerRequestDto);
+
+        //then
+        verify(customerRepository, times(1)).delete(any());
+    }
 }
